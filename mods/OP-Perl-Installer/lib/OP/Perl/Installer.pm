056e2380f0dd87c62dd53f8366af170b5d862dc4
op <op>
2013-01-28 22:12:31 +0200

    wip mkdist.pl

--- mods/OP-Perl-Installer/lib/OP/Perl/Installer.pm
+++ mods/OP-Perl-Installer/lib/OP/Perl/Installer.pm
@@ -33,6 +33,10 @@ use Data::Dumper;
 
 =cut
 
+=head3 new()
+
+=cut
+
 sub new()
 {
     my ($class, %parameters) = @_;
@@ -49,7 +53,11 @@ sub init(){
 	$self->{package_name}=__PACKAGE__ unless defined $self->{package_name};
 }
 
-# module_to_path(){{{
+# module_to_path() {{{
+
+=head3 module_to_path()
+
+=cut
 
 sub module_to_path(){
 	my $self=shift;
@@ -63,6 +71,11 @@ sub module_to_path(){
 }
 #}}}
 # module_to_def(){{{
+
+=head3 module_to_def()
+
+=cut
+
 sub module_to_def(){
 	my $self=shift;
 	# input: My::Module::Base
@@ -287,6 +300,11 @@ sub run_build_install(){
 }
 # }}}
 # sub run_shell(){{{
+
+=head3 run_shell()
+
+=cut
+
 sub run_shell(){
   my $self=shift;
   my $term = new Term::ShellUI(
