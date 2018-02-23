8068cfb7803782ed69d02e4714e144f3635ddeaa
op <op@localhost>
2018-02-23 10:45:31 +0200

    u

---
+++ lib/Vim/Xml.pm
@@ -0,0 +1,43 @@
+package Vim::Xml;
+
+use strict;
+use warnings;
+
+use warnings;
+use strict;
+
+use Exporter ();
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION = '0.01';
+@ISA     = qw(Exporter);
+
+@EXPORT      = qw();
+
+###export_vars_scalar
+my @ex_vars_scalar=qw(
+	$dom $domcache
+);
+###export_vars_hash
+my @ex_vars_hash=qw(
+);
+###export_vars_array
+my @ex_vars_array=qw(
+);
+
+%EXPORT_TAGS = (
+###export_funcs
+'funcs' => [qw( 
+)],
+'vars'  => [ @ex_vars_scalar,@ex_vars_array,@ex_vars_hash ]
+);
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'funcs'} }, @{ $EXPORT_TAGS{'vars'} } );
+our @EXPORT  = qw( );
+our $VERSION = '0.01';
+
+our ($domcache,$dom);
+
+1;
+ 
+
